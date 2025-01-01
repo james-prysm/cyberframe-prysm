leverj/prysm

Prysm rpc: Submit payload attestation data (#14381)

beacon-chain/node/node.go
@@ -965,55 +965,56 @@ func (b *BeaconNode) registerRPCService(router *http.ServeMux) error {
 
 	p2pService := b.fetchP2P()
 	rpcService := rpc.NewService(b.ctx, &rpc.Config{
-		ExecutionEngineCaller:     web3Service,
-		ExecutionReconstructor:    web3Service,
-		Host:                      host,
-		Port:                      port,
-		BeaconMonitoringHost:      beaconMonitoringHost,
-		BeaconMonitoringPort:      beaconMonitoringPort,
-		CertFlag:                  cert,
-		KeyFlag:                   key,
-		BeaconDB:                  b.db,
-		Broadcaster:               p2pService,
-		PeersFetcher:              p2pService,
-		PeerManager:               p2pService,
-		MetadataProvider:          p2pService,
-		ChainInfoFetcher:          chainService,
-		HeadFetcher:               chainService,
-		CanonicalFetcher:          chainService,
-		ForkFetcher:               chainService,
-		ForkchoiceFetcher:         chainService,
-		FinalizationFetcher:       chainService,
-		BlockReceiver:             chainService,
-		BlobReceiver:              chainService,
-		AttestationReceiver:       chainService,
-		GenesisTimeFetcher:        chainService,
-		GenesisFetcher:            chainService,
-		OptimisticModeFetcher:     chainService,
-		AttestationsPool:          b.attestationPool,
-		ExitPool:                  b.exitPool,
-		SlashingsPool:             b.slashingsPool,
-		BLSChangesPool:            b.blsToExecPool,
-		SyncCommitteeObjectPool:   b.syncCommitteePool,
-		ExecutionChainService:     web3Service,
-		ExecutionChainInfoFetcher: web3Service,
-		ChainStartFetcher:         chainStartFetcher,
-		MockEth1Votes:             mockEth1DataVotes,
-		SyncService:               syncService,
-		DepositFetcher:            depositFetcher,
-		PendingDepositFetcher:     b.depositCache,
-		BlockNotifier:             b,
-		StateNotifier:             b,
-		OperationNotifier:         b,
-		StateGen:                  b.stateGen,
-		EnableDebugRPCEndpoints:   enableDebugRPCEndpoints,
-		MaxMsgSize:                maxMsgSize,
-		BlockBuilder:              b.fetchBuilderService(),
-		Router:                    router,
-		ClockWaiter:               b.clockWaiter,
-		BlobStorage:               b.BlobStorage,
-		TrackedValidatorsCache:    b.trackedValidatorsCache,
-		PayloadIDCache:            b.payloadIDCache,
+		ExecutionEngineCaller:         web3Service,
+		ExecutionPayloadReconstructor: web3Service,
+		Host:                          host,
+		Port:                          port,
+		BeaconMonitoringHost:          beaconMonitoringHost,
+		BeaconMonitoringPort:          beaconMonitoringPort,
+		CertFlag:                      cert,
+		KeyFlag:                       key,
+		BeaconDB:                      b.db,
+		Broadcaster:                   p2pService,
+		PeersFetcher:                  p2pService,
+		PeerManager:                   p2pService,
+		MetadataProvider:              p2pService,
+		ChainInfoFetcher:              chainService,
+		HeadFetcher:                   chainService,
+		CanonicalFetcher:              chainService,
+		ForkFetcher:                   chainService,
+		ForkchoiceFetcher:             chainService,
+		FinalizationFetcher:           chainService,
+		BlockReceiver:                 chainService,
+		BlobReceiver:                  chainService,
+		PayloadAttestationReceiver:    chainService,
+		AttestationReceiver:           chainService,
+		GenesisTimeFetcher:            chainService,
+		GenesisFetcher:                chainService,
+		OptimisticModeFetcher:         chainService,
+		AttestationsPool:              b.attestationPool,
+		ExitPool:                      b.exitPool,
+		SlashingsPool:                 b.slashingsPool,
+		BLSChangesPool:                b.blsToExecPool,
+		SyncCommitteeObjectPool:       b.syncCommitteePool,
+		ExecutionChainService:         web3Service,
+		ExecutionChainInfoFetcher:     web3Service,
+		ChainStartFetcher:             chainStartFetcher,
+		MockEth1Votes:                 mockEth1DataVotes,
+		SyncService:                   syncService,
+		DepositFetcher:                depositFetcher,
+		PendingDepositFetcher:         b.depositCache,
+		BlockNotifier:                 b,
+		StateNotifier:                 b,
+		OperationNotifier:             b,
+		StateGen:                      b.stateGen,
+		EnableDebugRPCEndpoints:       enableDebugRPCEndpoints,
+		MaxMsgSize:                    maxMsgSize,
+		BlockBuilder:                  b.fetchBuilderService(),
+		Router:                        router,
+		ClockWaiter:                   b.clockWaiter,
+		BlobStorage:                   b.BlobStorage,
+		TrackedValidatorsCache:        b.trackedValidatorsCache,
+		PayloadIDCache:                b.payloadIDCache,
 	})
 
 	return b.services.RegisterService(rpcService)

james-prysm/cyberframe-prysm

beacon-chain/node/node.go
@@ -943,56 +943,57 @@ func (b *BeaconNode) registerRPCService(router *http.ServeMux) error {
 
 	p2pService := b.fetchP2P()
 	rpcService := rpc.NewService(b.ctx, &rpc.Config{
-		ExecutionEngineCaller:     web3Service,
-		ExecutionReconstructor:    web3Service,
-		Host:                      host,
-		Port:                      port,
-		BeaconMonitoringHost:      beaconMonitoringHost,
-		BeaconMonitoringPort:      beaconMonitoringPort,
-		CertFlag:                  cert,
-		KeyFlag:                   key,
-		BeaconDB:                  b.db,
-		Broadcaster:               p2pService,
-		PeersFetcher:              p2pService,
-		PeerManager:               p2pService,
-		MetadataProvider:          p2pService,
-		ChainInfoFetcher:          chainService,
-		HeadFetcher:               chainService,
-		CanonicalFetcher:          chainService,
-		ForkFetcher:               chainService,
-		ForkchoiceFetcher:         chainService,
-		FinalizationFetcher:       chainService,
-		BlockReceiver:             chainService,
-		BlobReceiver:              chainService,
-		AttestationReceiver:       chainService,
-		GenesisTimeFetcher:        chainService,
-		GenesisFetcher:            chainService,
-		OptimisticModeFetcher:     chainService,
-		AttestationCache:          b.attestationCache,
-		AttestationsPool:          b.attestationPool,
-		ExitPool:                  b.exitPool,
-		SlashingsPool:             b.slashingsPool,
-		BLSChangesPool:            b.blsToExecPool,
-		SyncCommitteeObjectPool:   b.syncCommitteePool,
-		ExecutionChainService:     web3Service,
-		ExecutionChainInfoFetcher: web3Service,
-		ChainStartFetcher:         chainStartFetcher,
-		MockEth1Votes:             mockEth1DataVotes,
-		SyncService:               syncService,
-		DepositFetcher:            depositFetcher,
-		PendingDepositFetcher:     b.depositCache,
-		BlockNotifier:             b,
-		StateNotifier:             b,
-		OperationNotifier:         b,
-		StateGen:                  b.stateGen,
-		EnableDebugRPCEndpoints:   enableDebugRPCEndpoints,
-		MaxMsgSize:                maxMsgSize,
-		BlockBuilder:              b.fetchBuilderService(),
-		Router:                    router,
-		ClockWaiter:               b.clockWaiter,
-		BlobStorage:               b.BlobStorage,
-		TrackedValidatorsCache:    b.trackedValidatorsCache,
-		PayloadIDCache:            b.payloadIDCache,
+		ExecutionEngineCaller:      web3Service,
+		ExecutionReconstructor:     web3Service,
+		Host:                       host,
+		Port:                       port,
+		BeaconMonitoringHost:       beaconMonitoringHost,
+		BeaconMonitoringPort:       beaconMonitoringPort,
+		CertFlag:                   cert,
+		KeyFlag:                    key,
+		BeaconDB:                   b.db,
+		Broadcaster:                p2pService,
+		PeersFetcher:               p2pService,
+		PeerManager:                p2pService,
+		MetadataProvider:           p2pService,
+		ChainInfoFetcher:           chainService,
+		HeadFetcher:                chainService,
+		CanonicalFetcher:           chainService,
+		ForkFetcher:                chainService,
+		ForkchoiceFetcher:          chainService,
+		FinalizationFetcher:        chainService,
+		BlockReceiver:              chainService,
+		BlobReceiver:               chainService,
+		AttestationReceiver:        chainService,
+		GenesisTimeFetcher:         chainService,
+		GenesisFetcher:             chainService,
+		OptimisticModeFetcher:      chainService,
+		AttestationCache:           b.attestationCache,
+		AttestationsPool:           b.attestationPool,
+		ExitPool:                   b.exitPool,
+		SlashingsPool:              b.slashingsPool,
+		BLSChangesPool:             b.blsToExecPool,
+		SyncCommitteeObjectPool:    b.syncCommitteePool,
+		ExecutionChainService:      web3Service,
+		ExecutionChainInfoFetcher:  web3Service,
+		ChainStartFetcher:          chainStartFetcher,
+		MockEth1Votes:              mockEth1DataVotes,
+		SyncService:                syncService,
+		DepositFetcher:             depositFetcher,
+		PendingDepositFetcher:      b.depositCache,
+		BlockNotifier:              b,
+		StateNotifier:              b,
+		OperationNotifier:          b,
+		StateGen:                   b.stateGen,
+		EnableDebugRPCEndpoints:    enableDebugRPCEndpoints,
+		MaxMsgSize:                 maxMsgSize,
+		BlockBuilder:               b.fetchBuilderService(),
+		Router:                     router,
+		ClockWaiter:                b.clockWaiter,
+		BlobStorage:                b.BlobStorage,
+		TrackedValidatorsCache:     b.trackedValidatorsCache,
+		PayloadIDCache:             b.payloadIDCache,
+		PayloadAttestationReceiver: chainService,
 	})
 
 	return b.services.RegisterService(rpcService)
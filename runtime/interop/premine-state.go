OffchainLabs/prysm

Add support to generate genesis state for epbs (#14594)

runtime/interop/premine-state.go
@@ -66,7 +66,7 @@ func NewPreminedGenesis(ctx context.Context, t, nvals, pCreds uint64, version in
 
 func (s *PremineGenesisConfig) prepare(ctx context.Context) (state.BeaconState, error) {
 	switch s.Version {
-	case version.Phase0, version.Altair, version.Bellatrix, version.Capella, version.Deneb, version.Electra:
+	case version.Phase0, version.Altair, version.Bellatrix, version.Capella, version.Deneb, version.Electra, version.EPBS:
 	default:
 		return nil, errors.Wrapf(errUnsupportedVersion, "version=%s", version.String(s.Version))
 	}
@@ -157,6 +157,11 @@ func (s *PremineGenesisConfig) empty() (state.BeaconState, error) {
 		}
 	case version.Electra:
 		e, err = state_native.InitializeFromProtoElectra(&ethpb.BeaconStateElectra{})
+		if err != nil {
+			return nil, err
+		}
+	case version.EPBS:
+		e, err = state_native.InitializeFromProtoEpbs(&ethpb.BeaconStateEPBS{})
 		if err != nil {
 			return nil, err
 		}
@@ -343,6 +348,8 @@ func (s *PremineGenesisConfig) setFork(g state.BeaconState) error {
 		pv, cv = params.BeaconConfig().CapellaForkVersion, params.BeaconConfig().DenebForkVersion
 	case version.Electra:
 		pv, cv = params.BeaconConfig().ElectraForkVersion, params.BeaconConfig().ElectraForkVersion
+	case version.EPBS:
+		pv, cv = params.BeaconConfig().EPBSForkVersion, params.BeaconConfig().EPBSForkVersion
 	default:
 		return errUnsupportedVersion
 	}
@@ -564,6 +571,31 @@ func (s *PremineGenesisConfig) setLatestBlockHeader(g state.BeaconState) error {
 				Consolidations: make([]*enginev1.ConsolidationRequest, 0),
 			},
 		}
+	case version.EPBS:
+		body = &ethpb.BeaconBlockBodyEpbs{
+			RandaoReveal: make([]byte, 96),
+			Eth1Data: &ethpb.Eth1Data{
+				DepositRoot: make([]byte, 32),
+				BlockHash:   make([]byte, 32),
+			},
+			Graffiti: make([]byte, 32),
+			SyncAggregate: &ethpb.SyncAggregate{
+				SyncCommitteeBits:      make([]byte, fieldparams.SyncCommitteeLength/8),
+				SyncCommitteeSignature: make([]byte, fieldparams.BLSSignatureLength),
+			},
+			SignedExecutionPayloadHeader: &enginev1.SignedExecutionPayloadHeader{
+				Message: &enginev1.ExecutionPayloadHeaderEPBS{
+					ParentBlockHash:        make([]byte, fieldparams.RootLength),
+					ParentBlockRoot:        make([]byte, fieldparams.RootLength),
+					BlockHash:              make([]byte, fieldparams.RootLength),
+					BlobKzgCommitmentsRoot: make([]byte, fieldparams.RootLength),
+				},
+				Signature: make([]byte, fieldparams.BLSSignatureLength),
+			},
+			BlsToExecutionChanges: make([]*ethpb.SignedBLSToExecutionChange, 0),
+			PayloadAttestations:   make([]*ethpb.PayloadAttestation, 0),
+		}
+
 	default:
 		return errUnsupportedVersion
 	}
@@ -712,6 +744,13 @@ func (s *PremineGenesisConfig) setExecutionPayload(g state.BeaconState) error {
 		if err != nil {
 			return err
 		}
+	case version.EPBS:
+		return g.SetLatestExecutionPayloadHeaderEPBS(&enginev1.ExecutionPayloadHeaderEPBS{
+			ParentBlockHash:        make([]byte, 32),
+			ParentBlockRoot:        make([]byte, 32),
+			BlockHash:              make([]byte, 32),
+			BlobKzgCommitmentsRoot: make([]byte, 32),
+		})
 	default:
 		return errUnsupportedVersion
 	}

james-prysm/cyberframe-prysm

runtime/interop/premine-state.go
@@ -66,7 +66,7 @@ func NewPreminedGenesis(ctx context.Context, t, nvals, pCreds uint64, version in
 
 func (s *PremineGenesisConfig) prepare(ctx context.Context) (state.BeaconState, error) {
 	switch s.Version {
-	case version.Phase0, version.Altair, version.Bellatrix, version.Capella, version.Deneb, version.Electra:
+	case version.Phase0, version.Altair, version.Bellatrix, version.Capella, version.Deneb, version.Electra, version.EPBS:
 	default:
 		return nil, errors.Wrapf(errUnsupportedVersion, "version=%s", version.String(s.Version))
 	}
@@ -157,6 +157,11 @@ func (s *PremineGenesisConfig) empty() (state.BeaconState, error) {
 		}
 	case version.Electra:
 		e, err = state_native.InitializeFromProtoElectra(&ethpb.BeaconStateElectra{})
+		if err != nil {
+			return nil, err
+		}
+	case version.EPBS:
+		e, err = state_native.InitializeFromProtoEpbs(&ethpb.BeaconStateEPBS{})
 		if err != nil {
 			return nil, err
 		}
@@ -343,6 +348,8 @@ func (s *PremineGenesisConfig) setFork(g state.BeaconState) error {
 		pv, cv = params.BeaconConfig().CapellaForkVersion, params.BeaconConfig().DenebForkVersion
 	case version.Electra:
 		pv, cv = params.BeaconConfig().DenebForkVersion, params.BeaconConfig().ElectraForkVersion
+	case version.EPBS:
+		pv, cv = params.BeaconConfig().ElectraForkVersion, params.BeaconConfig().EPBSForkVersion
 	default:
 		return errUnsupportedVersion
 	}
@@ -564,6 +571,31 @@ func (s *PremineGenesisConfig) setLatestBlockHeader(g state.BeaconState) error {
 				Consolidations: make([]*enginev1.ConsolidationRequest, 0),
 			},
 		}
+	case version.EPBS:
+		body = &ethpb.BeaconBlockBodyEpbs{
+			RandaoReveal: make([]byte, 96),
+			Eth1Data: &ethpb.Eth1Data{
+				DepositRoot: make([]byte, 32),
+				BlockHash:   make([]byte, 32),
+			},
+			Graffiti: make([]byte, 32),
+			SyncAggregate: &ethpb.SyncAggregate{
+				SyncCommitteeBits:      make([]byte, fieldparams.SyncCommitteeLength/8),
+				SyncCommitteeSignature: make([]byte, fieldparams.BLSSignatureLength),
+			},
+			SignedExecutionPayloadHeader: &enginev1.SignedExecutionPayloadHeader{
+				Message: &enginev1.ExecutionPayloadHeaderEPBS{
+					ParentBlockHash:        make([]byte, fieldparams.RootLength),
+					ParentBlockRoot:        make([]byte, fieldparams.RootLength),
+					BlockHash:              make([]byte, fieldparams.RootLength),
+					BlobKzgCommitmentsRoot: make([]byte, fieldparams.RootLength),
+				},
+				Signature: make([]byte, fieldparams.BLSSignatureLength),
+			},
+			BlsToExecutionChanges: make([]*ethpb.SignedBLSToExecutionChange, 0),
+			PayloadAttestations:   make([]*ethpb.PayloadAttestation, 0),
+		}
+
 	default:
 		return errUnsupportedVersion
 	}
@@ -712,6 +744,13 @@ func (s *PremineGenesisConfig) setExecutionPayload(g state.BeaconState) error {
 		if err != nil {
 			return err
 		}
+	case version.EPBS:
+		return g.SetLatestExecutionPayloadHeaderEPBS(&enginev1.ExecutionPayloadHeaderEPBS{
+			ParentBlockHash:        make([]byte, 32),
+			ParentBlockRoot:        make([]byte, 32),
+			BlockHash:              make([]byte, 32),
+			BlobKzgCommitmentsRoot: make([]byte, 32),
+		})
 	default:
 		return errUnsupportedVersion
 	}